253646aef78cdf58b736d8c38d4b4e2e7cfadccc
JayaniKodithuwakku <jayani24@stu.codepark.university>
2025-03-21 19:51:54 +0530

    created the SOAP service

--- tutorial1/src/main/java/com/example/tutorial1/StringManipulationService.java
+++
@@ -1,4 +0,0 @@
-package com.example.tutorial1;
-
-public interface StringManipulationService {
-}
--- web_19APC4362/src/main/java/com/example/web_19apc4362/HelloServlet.java
+++ web_19APC4362/src/main/java/com/example/web_19apc4362/HelloServlet.java
@@ -1,4 +1,4 @@
-package com.example.tutorial1;
+package com.example.web_19apc4362;
 
 import java.io.*;
 
